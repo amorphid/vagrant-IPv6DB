b096e87aa865fde8e984e25ff921a12496ba3aba
Michael Pope <mpope.cr@gmail.com>
2017-01-10 11:03:49 -0800

    Add test for find or create address by id

--- lib/vagrant/IPv6DB.rb
+++ lib/vagrant/IPv6DB.rb
@@ -2,6 +2,24 @@ require "vagrant/IPv6DB/version"
 
 module Vagrant
   module IPv6DB
-    # Your code goes here...
+    class << self
+      def find_or_create_address_by_id(id)
+        create_address
+      end
+
+      private
+
+      def address_count
+        @address_count ||= 0
+      end
+
+      def create_address
+        @address_count += 1
+      end
+
+      def delete_addresses
+        @address_count = 0
+      end
+    end
   end
 end
--- spec/vagrant/IPv6DB_spec.rb
+++ spec/vagrant/IPv6DB_spec.rb
@@ -1,11 +1,22 @@
 require "spec_helper"
 
 describe Vagrant::IPv6DB do
+
+  subject { described_class }
+
+  before { subject.send(:delete_addresses) }
+  after  { subject.send(:delete_addresses) }
+
   it "has a version number" do
     expect(Vagrant::IPv6DB::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "::find_or_create_address_by_id" do
+    it "increases address count by 1" do
+      expect { subject.find_or_create_address_by_id(1) }
+        .to change { subject.send(:address_count) }
+        .from(0)
+        .to(1)
+    end
   end
 end
