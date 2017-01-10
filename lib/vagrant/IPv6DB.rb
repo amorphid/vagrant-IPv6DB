ab15d11892452208ce8638b42d755b9bec8ea5b1
Michael Pope <mpope.cr@gmail.com>
2017-01-09 16:07:35 -0800

    Create initial commit

---
+++ lib/vagrant/IPv6DB.rb
@@ -0,0 +1,7 @@
+require "vagrant/IPv6DB/version"
+
+module Vagrant
+  module IPv6DB
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/IPv6DB/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module IPv6DB
+    VERSION = "0.1.0"
+  end
+end
